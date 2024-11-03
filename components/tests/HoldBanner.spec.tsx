e1c1ada0a2a5dc6d989fc6f519c813deec954057
Dillon Montag <dmontag23@gmail.com>
2024-11-03 20:43:06 +0000

    Fix integration tests

--- components/tests/HoldBanner.spec.tsx
+++ components/tests/HoldBanner.spec.tsx
@@ -130,11 +130,11 @@ describe("Hold banner", () => {
     // check the banner appears on the show details page
     await waitFor(() =>
       expect(
-        getByText("Attempting to get 1 ticket for Hamilton in 00:00:04")
+        getByText("Attempting to get 1 ticket for Hamilton in 00:00:03")
       ).toBeVisible()
     );
     const bannerText = getByText(
-      "Attempting to get 1 ticket for Hamilton in 00:00:04"
+      "Attempting to get 1 ticket for Hamilton in 00:00:03"
     );
     expect(getByText("Cancel")).toBeVisible();
     await userEvent.press(getByText("Cancel"));
@@ -220,7 +220,9 @@ describe("Hold banner", () => {
     await userEvent.press(getByText("2"));
 
     // check the banner appears on the show details page with an error message
-    expect(getByText("Trying to get 2 tickets to Hamilton")).toBeVisible();
+    await waitFor(() =>
+      expect(getByText("Trying to get 2 tickets to Hamilton")).toBeVisible()
+    );
     expect(getByTestId("loadingSpinner")).toBeVisible();
     await waitFor(() =>
       expect(
