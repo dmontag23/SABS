7fd7e99f9a8ed0ff57f495d4cf6067a791213b8e
Dillon Montag <dmontag23@gmail.com>
2024-11-24 00:46:17 +0000

    Remove disable Sentry env var

--- types/third-party-package-types/react-native-dotenv.d.ts
+++ types/third-party-package-types/react-native-dotenv.d.ts
@@ -1,6 +1,5 @@
 declare namespace NodeJS {
   export interface ProcessEnv {
-    DISABLE_SENTRY: string;
     TODAY_TIX_API_BASE_URL: string;
     TODAY_TIX_API_OAUTH_ENDPOINT: string;
     TODAY_TIX_API_V2_ENDPOINT: string;
