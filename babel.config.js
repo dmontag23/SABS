b1fbc75a051c37931a89e620e91ae8b39f163e98
Dillon Montag <dmontag23@gmail.com>
2025-07-18 09:08:07 +0100

    Fix babel config

--- babel.config.js
+++ babel.config.js
@@ -2,16 +2,17 @@ module.exports = {
   presets: ["module:@react-native/babel-preset"],
   /* The following is needed for react-native-dotenv and react-native-reanimated. The reanimated plugin must be listed last!
   See https://github.com/goatandsheep/react-native-dotenv and https://docs.swmansion.com/react-native-reanimated/docs/fundamentals/getting-started */
-  plugins: ["module:react-native-dotenv", "react-native-reanimated/plugin"],
+  plugins: [
+    "@babel/plugin-transform-export-namespace-from",
+    "module:react-native-dotenv",
+    "react-native-reanimated/plugin"
+  ],
   /** The following is needed for react native paper.
    *  See https://callstack.github.io/react-native-paper/docs/guides/getting-started
    */
   env: {
     production: {
-      plugins: [
-        "@babel/plugin-transform-export-namespace-from",
-        "react-native-paper/babel"
-      ]
+      plugins: ["react-native-paper/babel"]
     }
   }
 };
