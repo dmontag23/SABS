13fe4ea4e9705cd9b7d409ac2cf2f6ad3db6496f
Dillon Montag <dmontag23@gmail.com>
2026-02-19 10:03:00 +0100

    Fix access and refresh token input finding in e2e tests

--- tests/e2e/utils/utils.ts
+++ tests/e2e/utils/utils.ts
@@ -5,14 +5,10 @@ for every test isn't great. Something like a deep link may work better, but
 has other disadvantages. Perhaps exposing a way to do this from the mock server somehow
 would be best? */
 export const login = async () => {
-  const accessTokenFormInput = element(by.label("Access token input")).atIndex(
-    1
-  );
+  const accessTokenFormInput = element(by.label("Access token input"));
   await expect(accessTokenFormInput).toBeVisible();
   await accessTokenFormInput.typeText("access-token");
-  await element(by.label("Refresh token input"))
-    .atIndex(1)
-    .typeText("refresh-token");
+  await element(by.label("Refresh token input")).typeText("refresh-token");
   await element(by.text("Login")).tap();
 };
 
